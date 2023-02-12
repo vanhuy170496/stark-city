5e8e7a4bfd9f29e9b542621b327a671c4cfdc399
huynv4 <huynv4@vng.com.vn>
2023-02-12 22:54:28 +0700

    update https

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,18 @@ import { HomePage } from './pages/Home';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import { AdminPage } from './pages/Admin';
+import { useEffect } from 'react';
 
 function App() {
   const url = new URL(window.location.href);
+
+  useEffect(() => {
+    const url = new URL(window.location.href);
+    if (url.protocol !== 'https:' && !url.host.includes('localhost')) {
+      window.location.href = window.location.href.replace('http:', 'https:');
+    }
+  }, []);
+
   if (url.searchParams.get('admin1') === '1') {
     return <AdminPage />;
   }
